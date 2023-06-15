3d02f17759803c8a5f5fd0fafee3909845877ca4
c4russian <martinmonzon00@gmail.com>
2023-06-14 22:16:04 -0400

    drop_all and resert seq

--- drop_all.py
+++ drop_all.py
@@ -22,6 +22,20 @@ cursor.execute('SET session_replication_role = replica;')
 for table_name in table_names:
     cursor.execute(f'TRUNCATE TABLE flea_mktv.{table_name} RESTART IDENTITY CASCADE;')
 
+# Reset each sequence to start from 1
+sequences = [
+    "flea_mktv.category_categories_id_seq",
+    "flea_mktv.item_item_id_seq2",
+    "flea_mktv.offer_offer_id_seq2",
+    "flea_mktv.player_player_id_seq2",
+    "flea_mktv.product_product_id_seq2",
+    "flea_mktv.tax_tax_id_seq2",
+    "flea_mktv.transaction_transaction_id_seq2"
+]
+
+for sequence in sequences:
+    cursor.execute(f"ALTER SEQUENCE {sequence} RESTART WITH 1;")
+
 # Enable foreign key checks again
 cursor.execute('SET session_replication_role = DEFAULT;')
 
